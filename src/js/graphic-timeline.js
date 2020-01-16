472398dfc577e4a63085bc313e899a29853af0d5
Amber Thomas <ProQuestionAsker@gmail.com>
2020-01-16 11:08:33 -0800

    loading data

--- src/js/graphic-timeline.js
+++ src/js/graphic-timeline.js
@@ -1,11 +1,18 @@
+import loadData from './load-data';
+import './pudding-chart/timeline';
+
 const $section = d3.select('[data-js="timeline"]');
 const $figure = $section.selectAll('[data-js="timeline__figure"]');
 // const $article = $section.select('[data-js="distance__article"]');
 // const $step = $article.selectAll('[data-js="article__step"]');
 
-function resize() { }
+function setup(data) {
+  console.log({ data });
+}
+
+function resize() {}
 function init() {
-    console.log($figure);
+  loadData(['authors.json', 'books.json']).then(setup);
 }
 
 export default { init, resize };
