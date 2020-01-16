b7b2ac6b44f0f48da8eb31fdaf949e3ca190b466
Amber Thomas <ProQuestionAsker@gmail.com>
2020-01-16 11:14:51 -0800

    cleaning data

--- src/js/graphic-timeline.js
+++ src/js/graphic-timeline.js
@@ -6,13 +6,32 @@ const $figure = $section.selectAll('[data-js="timeline__figure"]');
 // const $article = $section.select('[data-js="distance__article"]');
 // const $step = $article.selectAll('[data-js="article__step"]');
 
-function setup(data) {
-  console.log({ data });
+function cleanBooks(data) {
+    const clean = data.map(d => ({
+        ...d,
+        pub_year: +d.pub_year,
+    }));
+    return clean;
 }
 
-function resize() {}
+function cleanAuthors(data) {
+    const clean = data.map(d => ({
+        ...d,
+        start_year: +d.start_year,
+        end_year: +d.end_year,
+    }));
+    return clean;
+}
+
+function setup(data) {
+    const authors = cleanAuthors(data[0]);
+    const books = cleanBooks(data[1]);
+    console.log({ authors, books });
+}
+
+function resize() { }
 function init() {
-  loadData(['authors.json', 'books.json']).then(setup);
+    loadData(['authors.json', 'books.json']).then(setup);
 }
 
 export default { init, resize };
